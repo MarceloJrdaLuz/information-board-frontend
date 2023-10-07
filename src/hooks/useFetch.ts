b2cc91a8a87e53e4973e30febe6dc3083a494e2d
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2023-10-07 12:42:38 -0300

    update

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 import { api } from "@/services/api"
 import useSWR from "swr"
 
-export function useFetch<Data = any, Error = any>(url: string) {
+export function useFetch<Data = unknown, Error = any>(url: string) {
     const { data, error, mutate } = useSWR<Data, Error>(url !== '' ? url : null, async url => {
         const response = await api.get(url)
 
