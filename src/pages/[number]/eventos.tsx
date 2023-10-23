44b2f413509508aecfd0141c5f119d24ec0811b4
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2023-10-23 11:44:19 -0300

    update

--- src/pages/[number]/eventos.tsx
+++ src/pages/[number]/eventos.tsx
@@ -3,11 +3,9 @@ import LayoutPrincipal from "@/Components/LayoutPrincipal"
 import PdfViewer from "@/Components/PdfViewer"
 import { domainUrl } from "@/atoms/atom"
 import { usePublicDocumentsContext } from "@/context/PublicDocumentsContext"
-import { Categories, CongregationTypes, IDocument } from "@/entities/types"
+import { Categories, ICongregation, IDocument } from "@/entities/types"
 import { removeMimeType } from "@/functions/removeMimeType"
-import { api } from "@/services/api"
 import { useAtomValue } from "jotai"
-import { GetServerSideProps } from "next"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 import iconEvents from '../../../public/images/eventos-gray.png'
@@ -15,25 +13,23 @@ import Image from "next/image"
 import Button from "@/Components/Button"
 import { ChevronsLeftIcon } from "lucide-react"
 import NotFoundDocument from "@/Components/NotFoundDocument"
+import { useFetch } from "@/hooks/useFetch"
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { number } = context.query
-
-    const getCongregation = await api.get(`/congregation/${number}`)
-
-    const { data: congregationData } = getCongregation
-
-    return {
-        // Passed to the page component as props
-        props: { ...congregationData },
-    }
-}
-
-export default function Eventos({ circuit: congregationCircuit, name: congregationName, number: congregationNumber }: CongregationTypes) {
-
+export default function Eventos() {
     const router = useRouter()
     const { number } = router.query
     const domain = useAtomValue(domainUrl)
+
+    const [congregationData, setCongregationData] = useState<ICongregation>()
+
+    const fetchConfigCongregationData = number ? `/congregation/${number}` : ""
+    const { data: congregation } = useFetch<ICongregation>(fetchConfigCongregationData)
+
+    useEffect(() => {
+        if (congregation) {
+            setCongregationData(congregation)
+        }
+    }, [congregation])
 
     const { setCongregationNumber, documents, filterDocuments } = usePublicDocumentsContext()
 
@@ -63,20 +59,20 @@ export default function Eventos({ circuit: congregationCircuit, name: congregati
             <HeadComponent title="Eventos" urlMiniatura={`${domain}/images/eventos.png`} />
             <LayoutPrincipal image={
                 <Image src={iconEvents} alt="Icone de um calendário" fill />
-            } congregationName={congregationName} circuit={congregationCircuit} textoHeader="Eventos" heightConteudo={'1/2'} header className="bg-cartas bg-left-bottom bg-cover lg:bg-right">
+            } congregationName={congregationData?.name ?? ""} circuit={congregationData?.circuit ?? ""} textoHeader="Eventos" heightConteudo={'1/2'} header className="bg-cartas bg-left-bottom bg-cover lg:bg-right">
                 <div className="linha bg-gray-500 mt-2 w-full h-0.5 md:w-8/12 my-0 m-auto"></div>
                 <div className="flex justify-between overflow-auto hide-scrollbar w-11/12 md:w-8/12 gap-2 my-2 m-auto flex-wrap">
-                {documentsFilter && documentsFilter?.length > 0 ? documentsFilter?.map(document => (
+                    {documentsFilter && documentsFilter?.length > 0 ? documentsFilter?.map(document => (
                         <Button className="w-full" key={document.id} onClick={() => { handleButtonClick(document.url) }}>
                             {removeMimeType(document.fileName)}
                         </Button>
                     )) : (
-                        <NotFoundDocument message="Nenhum evento especial encontrado!"/>
+                        <NotFoundDocument message="Nenhum evento especial encontrado!" />
                     )
                     }
                 </div>
                 <Button
-                    onClick={() => router.push(`/${congregationNumber}`)}
+                    onClick={() => router.push(`/${number}`)}
                     className="w-1/2 mx-auto"
                 ><ChevronsLeftIcon />Voltar</Button>
             </LayoutPrincipal>
--- src/pages/[number]/financeiro.tsx
+++ src/pages/[number]/financeiro.tsx
@@ -3,11 +3,9 @@ import LayoutPrincipal from "@/Components/LayoutPrincipal"
 import PdfViewer from "@/Components/PdfViewer"
 import { domainUrl } from "@/atoms/atom"
 import { usePublicDocumentsContext } from "@/context/PublicDocumentsContext"
-import { Categories, CongregationTypes, ICongregation, IDocument } from "@/entities/types"
+import { Categories, ICongregation, IDocument } from "@/entities/types"
 import { removeMimeType } from "@/functions/removeMimeType"
-import { api } from "@/services/api"
 import { useAtomValue } from "jotai"
-import { GetServerSideProps } from "next"
 import Image from "next/image"
 import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
@@ -15,26 +13,25 @@ import iconeFinanceiro from '../../../public/images/financeiro-gray.png'
 import Button from "@/Components/Button"
 import { ChevronsLeftIcon } from "lucide-react"
 import NotFoundDocument from "@/Components/NotFoundDocument"
+import { useFetch } from "@/hooks/useFetch"
 
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    const { number } = context.query
-
-    const getCongregation = await api.get(`/congregation/${number}`)
-
-    const { data: congregationData } = getCongregation
-
-    return {
-        // Passed to the page component as props
-        props: { ...congregationData },
-    }
-}
-
-export default function Financeiro({ circuit: congregationCircuit, name: congregationName, number: congregationNumber }: CongregationTypes) {
+export default function Financeiro() {
 
     const router = useRouter()
     const { number } = router.query
     const domain = useAtomValue(domainUrl)
+
+    const [congregationData, setCongregationData] = useState<ICongregation>()
+
+    const fetchConfigCongregationData = number ? `/congregation/${number}` : ""
+    const { data: congregation } = useFetch<ICongregation>(fetchConfigCongregationData)
+
+    useEffect(() => {
+        if (congregation) {
+            setCongregationData(congregation)
+        }
+    }, [congregation])
+
 
     const { setCongregationNumber, documents, filterDocuments } = usePublicDocumentsContext()
 
@@ -64,20 +61,20 @@ export default function Financeiro({ circuit: congregationCircuit, name: congreg
             <HeadComponent title="Financeiro" urlMiniatura={`${domain}/images/financeiro.png`} />
             <LayoutPrincipal image={
                 <Image src={iconeFinanceiro} alt="Icone uma calculadora e contas" fill />
-            } congregationName={congregationName} circuit={congregationCircuit} heightConteudo={'1/2'} header className="bg-contas bg-left-bottom bg-cover lg:bg-right" textoHeader="Relatório Financeiro">
+            } congregationName={congregationData?.name ?? ""} circuit={congregationData?.circuit ?? ""} heightConteudo={'1/2'} header className="bg-contas bg-left-bottom bg-cover lg:bg-right" textoHeader="Relatório Financeiro">
                 <div className="linha bg-gray-500 mt-2 w-full h-0.5 md:w-8/12 my-0 m-auto"></div>
                 <div className="flex justify-between overflow-auto hide-scrollbar w-11/12 md:w-8/12 gap-2 my-2 m-auto flex-wrap">
-                {documentsFilter && documentsFilter?.length > 0 ? documentsFilter?.map(document => (
+                    {documentsFilter && documentsFilter?.length > 0 ? documentsFilter?.map(document => (
                         <Button className="w-full" key={document.id} onClick={() => { handleButtonClick(document.url) }}>
                             {removeMimeType(document.fileName)}
                         </Button>
                     )) : (
-                        <NotFoundDocument message="Relatório das contas não localizado!"/>
+                        <NotFoundDocument message="Relatório das contas não localizado!" />
                     )
                     }
                 </div>
                 <Button
-                    onClick={() => router.push(`/${congregationNumber}`)}
+                    onClick={() => router.push(`/${number}`)}
                     className="w-1/2 mx-auto"
                 ><ChevronsLeftIcon />Voltar</Button>
             </LayoutPrincipal >
