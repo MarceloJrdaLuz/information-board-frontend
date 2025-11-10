c8cd968d3fc9018a1ccb76f1b71904c14cef475a
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2025-11-10 18:58:42 -0300

    update skeleton

--- src/Components/Footer/skeleton.tsx
+++ src/Components/Footer/skeleton.tsx
@@ -1,8 +1,8 @@
 export function FooterPublicSkeleton() {
     return (
-        <footer className="bg-primary-200 text-white py-6 text-center animate-pulse">
-            <div className="h-4 w-1/3 bg-primary-100 mx-auto mb-2 rounded shimmer" />
-            <div className="h-3 w-1/2 bg-primary-100 mx-auto rounded shimmer" />
+        <footer className="bg-primary-200 text-white py-6 text-center ">
+            <div className="h-4 w-1/3 bg-primary-100 mx-auto mb-2 rounded shimmer-primary-color" />
+            <div className="h-3 w-1/2 bg-primary-100 mx-auto rounded shimmer-primary-color" />
         </footer>
     )
 }
--- src/Components/Header/skeleton.tsx
+++ src/Components/Header/skeleton.tsx
@@ -1,7 +1,7 @@
 export function HeaderPublicSkeleton() {
   return (
     <div className="relative flex justify-center items-center h-[40vh] w-full bg-typography-800 animate-pulse">
-      <div className="h-full w-full max-w-[600px] bg-typography-700 shimmer" />
+      <div className="h-full w-full max-w-[600px] bg-typography-700" />
       <span className="absolute text-3xl text-typography-400"></span>
     </div>
   )
