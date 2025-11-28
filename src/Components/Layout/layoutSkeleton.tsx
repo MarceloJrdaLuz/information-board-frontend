e5cf9aee89cb2d27fe139c56ac14051825e2998d
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2025-11-28 13:20:09 -0300

    bug-fix

--- src/Components/Layout/layoutSkeleton.tsx
+++ src/Components/Layout/layoutSkeleton.tsx
@@ -4,13 +4,14 @@ export default function LayoutSkeleton() {
     return (
         <main className="flex w-screen h-screen overflow-hidden bg-secondary-100">
             {/* NAVBAR LATERAL */}
-            <aside className="hidden bg-primary-200 shadow-lg w-2/3 md:w-2/12 md:flex md:min-w-[185px] md:flex-col pt-6 animate-pulse">
+            <aside className="hidden bg-primary-200 shadow-lg w-2/3 md:w-2/12 md:flex md:min-w-[185px] md:flex-col animate-pulse">
                 {/* Logo / Título */}
-                <div className="text-surface-300">
-                    <NavBar.Logo isMenuOpen />
+                <div className="flex flex-col">
+                        <div className="text-surface-300">
+                            <NavBar.Logo isMenuOpen />
+                        </div>
+                    <div><NavBar.Skeleton className="mt-20" items={5} /></div>
                 </div>
-
-                <NavBar.Skeleton items={5} />
             </aside>
 
             {/* CONTEÚDO PRINCIPAL */}
