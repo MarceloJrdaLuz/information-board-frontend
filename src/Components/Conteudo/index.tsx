1aee7f581f44641ac0bec6ca023d97fe83e16dfa
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2025-11-29 20:57:36 -0300

    update

--- src/Components/Conteudo/index.tsx
+++ src/Components/Conteudo/index.tsx
@@ -7,7 +7,7 @@ interface ConteudoProps{
 }
 export default function Conteudo (props: ConteudoProps){
     return(
-        <div className={`conteudo overflow-y-auto hide-scrollbar text-center flex flex-col ${props.hConteudo && props.hConteudo} min-h-[50vh] ${props.bgFundo ? props.bgFundo : 'bg-surface-200' } p-2 gap-6 ${props.justifyContent ? props.justifyContent : 'justify-around'} ${props.hConteudo === 'full' && 'min-h-screen'}`}>
+        <div className={`conteudo pb-8 pt-5 px-3 gap-6 overflow-y-auto hide-scrollbar text-center flex flex-col ${props.hConteudo && props.hConteudo} min-h-[50vh] ${props.bgFundo ? props.bgFundo : 'bg-surface-200' }  ${props.justifyContent ? props.justifyContent : 'justify-around'} ${props.hConteudo === 'full' && 'min-h-screen'}`}>
             {props.children}
         </div>
     )
