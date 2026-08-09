80d981646521b8a7b836d81757df4d441968cb2c
MarceloJrdaLuz <marcelojuniordaluz@gmail.com>
2026-08-08 22:41:02 -0300

    feature: acrescentado recurso para instalar como aplicativo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,40 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta
+          name="theme-color"
+          content="#208a88"
+        />
+
+        <meta
+          name="mobile-web-app-capable"
+          content="yes"
+        />
+
+        <meta
+          name="apple-mobile-web-app-capable"
+          content="yes"
+        />
+
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="default"
+        />
+
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Information Board"
+        />
+
+        <link
+          rel="apple-touch-icon"
+          href="/images/pwa-192.png"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
